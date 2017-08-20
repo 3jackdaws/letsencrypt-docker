c008b15a89dd5357ab91cde3e2fca5928acad560
Ian Murphy <3jackdaws@gmail.com>
2017-08-20 14:49:23 -0700

    cleanup 2

--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ def write_site_config(server_name, ip, port=80):
     filename = SITE_CONFIG_DIR + "/" + server_name + ".conf"
     if not os.path.isfile(filename):
         config = generate_nginx_site_config(server_name, ip, port)
-        with open(filename) as fp:
+        with open(filename, "w+") as fp:
             fp.write(config)
     else:
         print("Site config already exists.  Skipping.")
@@ -46,7 +46,10 @@ def get_cert(domain, email):
 
 def move_cert(domain):
     final_dir = KEY_OUTPUT_DIR + "/" + domain
-    os.rmdir(final_dir)
+    try:
+        os.rmdir(final_dir)
+    except:
+        pass
     check_call("cp -R {} {}".format("/etc/letsencrypt/archive/" + domain, final_dir))
 
 
