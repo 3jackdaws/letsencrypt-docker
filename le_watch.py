8f4fa34857384f416fd708679e2cfd94f3f6df73
Ian Murphy <3jackdaws@gmail.com>
2017-08-19 22:49:11 -0700

    Better version

--- le_watch.py
+++ le_watch.py
@@ -1,97 +1,51 @@
-import time
+
 import os
-from ruamel.yaml import round_trip_dump, round_trip_load
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, FileModifiedEvent
 from subprocess import check_output
 
-CONFIG_FILE = "/config/config.yaml"
+ENVIRONMENT_VARIABLES = [
+    "webroot",
+    "email",
+    "subdomains",
+    "domain",
+    "subdomains_only"
+]
 
-CONFIG_FILE = os.path.realpath(CONFIG_FILE)
-
-CONFIG_TEXT = """
-webroot: /var/www
-status: ready
-email: example@example.com
-domains:
-    - example.com
-    - example2.com
-"""
-
-CONFIG = {}
-
-def load_config():
-
-    try:
-        temp = round_trip_load(open(CONFIG_FILE))
-        if "email" not in temp:
-            raise
-    except Exception as e:
-        print(e)
-        with open(CONFIG_FILE, "w+") as fp:
-            fp.write(CONFIG_TEXT)
-            fp.seek(0)
-            temp = round_trip_load(fp)
-    return temp
-
-def reset_config_status():
-    try:
-        conf = load_config()
-        conf['status'] = "updated"
-        with open(CONFIG_FILE, "w+") as fp:
-            round_trip_dump(conf, fp)
-        return True
-    except:
-        return False
+ENV = {}
+for varname in ENVIRONMENT_VARIABLES:
+    ENV[varname] = os.environ.get(varname.upper())
 
 
-def get_certs_for_domains(domains):
+
+def get_certs_for_domains(subdomains):
+    domain = ENV['domain']
+    email = ENV['email']
+    webroot = ENV['webroot']
+    domains = []
+
+    if ENV['subdomains_only'].lower() is not "true":
+        domains.append(domain)
+
+    for subdomain in subdomains:
+        domains.append(subdomain + "." + domain)
+
     for domain in domains:
         cmd = "letsencrypt certonly --dry-run --webroot --agree-tos -m{email} -w{webroot} -d {domain}".format(
-            email  =CONFIG['email'],
-            webroot=CONFIG['webroot'],
+            email  =email,
+            webroot=webroot,
             domain =domain
         )
-        print(cmd)
-        # check_output(cmd, shell=True)
+        check_output(cmd, shell=True)
 
-def ready_to_update():
-    try:
-        status = round_trip_load(open(CONFIG_FILE))['status']
-        return status.lower() == "ready"
-    except Exception as e:
-        print(e)
-        return False
-
-
-class Handler(FileSystemEventHandler):
-    def on_any_event(self, event):
-        global CONFIG
-        if isinstance(event, FileModifiedEvent):
-            if CONFIG_FILE == os.path.realpath(event.src_path):
-                if ready_to_update():
-                    print("{} modified.".format(event.src_path))
-                    previous_domains = set(CONFIG['domains'])
-                    CONFIG = load_config()
-                    new_domains = set(CONFIG['domains'])
-                    different_domains = [d for d in new_domains if d not in previous_domains]
-                    get_certs_for_domains(different_domains)
-                    reset_config_status()
 
 
 if __name__ == "__main__":
-    CONFIG = load_config()
-    get_certs_for_domains(CONFIG['domains'])
-    reset_config_status()
-    event_handler = Handler()
-    observer = Observer()
-    observer.schedule(event_handler, ".", recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(2)
-    except:
-        observer.stop()
-        print("Error")
 
-    observer.join()
+    # make sure these environment variables are set
+    for var in ["webroot", "email", "subdomains", "domain"]:
+        if var not in ENV or not ENV[var]:
+            print("Missing environment variable: {}".format(var.upper()))
+            exit(1)
+
+    # split subdomains on comma and pass
+    get_certs_for_domains(ENV['subdomains'].split(","))
+
